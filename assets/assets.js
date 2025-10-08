195d02efdbcfbf3d9b4612fb1f02cc00b3ba9a67
receptivepearl <rsudhig@gmail.com>
2025-10-08 16:19:58 -0700

    Your commit message

--- assets/assets.js
+++ assets/assets.js
@@ -281,9 +281,7 @@ export const userDummyData = {
   "name": "GreatStack",
   "email": "admin@example.com",
   "imageUrl": "https://img.clerk.com/eyJ0eXBlIjoiZGVmYXVsdCIsImlpZCI6Imluc18ycnlnUnFiUDBYT2dEZ2h1ZmRXcGlpdWV5OXoiLCJyaWQiOiJ1c2VyXzJzWkZIUzFVSUl5c0p5RFZ6Q3BRaFVoVElodyJ9",
-  "cartItems": {
-    // "67a1f4e43f34a77b6dde9144": 3
-  },
+  "cartItems": {},
   "__v": 0
 }
 
