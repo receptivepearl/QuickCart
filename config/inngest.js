3997c5e159acb0c8704ecf370fdd64b8ba1c07bd
receptivepearl <rsudhig@gmail.com>
2025-10-08 13:35:11 -0700

    Fix Inngest syntax error

--- config/inngest.js
+++ config/inngest.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 export const syncUserCreation = inngest.createFunction(
     {
         id: 'sync-user-from-clerk'
-    }
+    },
     {event: 'clerk/user.created'},
     async ({event}) => {
         const {id, first_name, last_name, email_addresses, image_url} = event.data
