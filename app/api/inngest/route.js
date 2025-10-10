8d6f246a2fa129c44cd3790fe34dd8e7d3bda494
receptivepearl <rsudhig@gmail.com>
2025-10-10 15:00:35 -0700

    Fix: import inngest to resolve ReferenceError on Vercel

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,7 @@
 import { serve } from "inngest/next";
 import { syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/config/inngest";
 import { createUserOrder } from "@/config/inngest";  
+import { inngest } from "inngest/next";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
