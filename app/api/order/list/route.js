a510f81d45a761d3151fc873ed9921d23518e2eb
receptivepearl <rsudhig@gmail.com>
2025-10-10 15:00:55 -0700

    Fix: import inngest to resolve ReferenceError on Vercel

---
+++ app/api/order/list/route.js
@@ -0,0 +1,22 @@
+// import { getAuth } from "@clerk/nextjs/server"
+// import Address from "@/models/Address"
+// import connectDB from "@/config/db"
+// import Order from "@/models/Order"
+// import Product from "@/models/Product"
+
+
+// export async function GET(request) {
+//     try{
+//         const{userId} = getAuth(request)
+
+//         await connectDB()
+//         Address.length
+//         Product.length
+
+//         const orders = await Order.find()
+
+//     } catch(error) {
+
+//     }
+    
+// }
